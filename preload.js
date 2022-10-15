dd254e91d94953b3d4c1c3a288cd60a934ade422
tamakiii <tamakiii@users.noreply.github.com>
2022-10-15 16:46:51 +0900

    calling crypto.createHash() via contextBridge

--- preload.js
+++ preload.js
@@ -1,8 +1,17 @@
 const { contextBridge, ipcRenderer } = require('electron')
+const crypto = require('node:crypto')
 
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
   ping: () => ipcRenderer.invoke('ping'),
+})
+
+contextBridge.exposeInMainWorld('nodeCrypto', {
+  sha256sum (data) {
+    const hash = crypto.createHash('sha256')
+    hash.update(data)
+    return hash.digest('hex')
+  }
 })
